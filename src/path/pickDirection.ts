0d85d8e041f74f079c83262e0417eea224d7405e
Mario Krajacic <marioo.krajacic@gmail.com>
2024-07-11 10:02:32 +0200

    Refactor pickDirection

--- src/path/pickDirection.ts
+++ src/path/pickDirection.ts
@@ -9,48 +9,49 @@ export function pickDirection(
   currentElement: string,
   options: MoveOption[],
 ): Direction {
+  // cannot continue if elemnt is invalid or no valid options available
   if (!currentElement.match(/^[A-Z|+-@]$/)) throw new ElementInvalidError();
 
+  // no options provided
   if (options.length === 0) throw new PathInvalidError("Broken path");
 
+  // remove unvisited intersections from options
+  // this is here only to throw an error distinguishing broken path and no valid moves
   options = options.filter(
     (opt) => !isIntersection(opt.element, opt.direction) || opt.isVisited,
   );
   if (options.length === 0) throw new PathInvalidError("No valid moves");
 
-  let opt: MoveOption;
-
   switch (currentElement) {
+    // in case of straight elements, prefer keeping direction
     case "|":
     case "-":
-      opt = options[0];
-      break;
+      return options[0].direction;
+
+    // in case of turns, straight and forks not allowed
     case "+":
       if (options.length === 1 && options[0].turn === 0)
         throw new PathInvalidError("Fake turn");
       options = options.filter((opt) => opt.turn !== 0);
       if (options.length > 1) throw new PathInvalidError("Fork");
-      opt = options[0];
-      break;
+      return options[0].direction;
     case "@":
+      // starting point is only allowed to have one option
+      // this is distinct from straight elements case because getValidTurns limits their options to one,
+      // compared to starting point, which can move in any direction
       if (options.length > 1)
         throw new PathInvalidError("Multiple starting points");
-      opt = options[0];
-      break;
+      return options[0].direction;
     default:
       // letters
-      // take the only option or prefer straight option. Note unvisited intersections will be filtered out already
+      // TODO simplify
+      // Take the only option OR take straight option OR take only unvisited option. Throw error on no valid options or fork
       if (options.length === 1 || options[0].turn === 0) {
-        opt = options[0];
-        break;
+        return options[0].direction;
       }
-
-      // multiple paths? remove visited
       options = options.filter((opt) => !opt.isVisited);
       if (options.length === 0) throw new PathInvalidError("No valid options");
       if (options.length > 1) throw new PathInvalidError("Multiple paths");
-      opt = options[0];
+      return options[0].direction;
   }
-
-  return opt.direction;
 }
